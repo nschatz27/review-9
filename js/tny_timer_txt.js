89455209ba73ba56238ff29ea0a3954dd3c429c2
nschatz27 <akschatz1s@capetigers.com>
2026-01-22 09:57:39 -0600

    finish review

--- js/tny_timer_txt.js
+++ js/tny_timer_txt.js
@@ -1,3 +1,6 @@
+
+"use script";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -9,14 +12,30 @@
 
 */
 
-showClock;
+showClock();
+setInterval("showClock(), 1000");
 
-var currentDay = new Date("May 19, 2018 9:31:27 a.m.");
-var dateStr = currentDay.toLocaleDateString();
-var timeStr = currentDay.toLocaleTimeString();
+function showClock() {
 
-var currentDay = new Date();
-document.getElementById ("dateNow").innerHTML = "<br /><span>date</span><span>time</span>"; 
+var thisDay = new Date();
+var localDate = thisDay.toLocaleDateString();
+var localTime = thisDay.toLocaleTimeString();
+
+document.getElementById ("currentTime").innerHTML = localDate + "<br/>" + localTime;
+
+var j4Date = nextJuly4(thisDay);
+j4Date.setHours(21);
+
+var days = (j4Date - thisDay)/(1000*60*60*24);
+var hrs = (days - Math.floor (days)) *24;
+var mins = (hrs - Math.floor(hrs)) *60;
+var secs = (mins - Math.floor (mins)) *60;
+
+document.getElementById("dLeft").textContent = Math.floor(days);
+document.getElementById("hLeft").textContent = Math.floor(hrs);
+document.getElementById("mLeft").textContent = Math.floor(mins);
+document.getElementById("sLeft").textContent = Math.floor(secs);
+}
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
